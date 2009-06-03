feb2efac8e05550d4ac1c28d926aa098dfc693d0
pete@shinners.org <pete@shinners.org@122de0bc-1ed4-11de-896d-b39e94f1936c>
2009-06-03 05:32:45 +0000

    condense to single module from a package

--- reimport.py
+++ reimport.py
@@ -22,6 +22,12 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
+"""
+This module intends to be a full featured replacement for Python's reload
+function. It is targeted towards making a reload that works for Python
+plugins and extensions used by longer running applications.
+"""
+
 
 __all__ = ["reimport", "modified"]
 
@@ -33,6 +39,15 @@ import inspect
 import weakref
 import traceback
 import time
+
+
+
+__version__ = "0.9"
+__author__ = "Peter Shinners <pete@shinners.org>"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2009 Peter Shinners"
+__url__ = "http://code.google.com/p/reimport"
+
 
 
 _previous_scan_time = time.time() - 1.0
@@ -103,8 +118,6 @@ def reimport(*modules):
             continue
         
         compile(data, pyname, "exec", 0, False)  # Let this raise exceptions
-
-    # XXX need to remove extensions out of the list now
 
     # Move modules out of sys
     oldModules = {}
@@ -366,8 +379,8 @@ def _rejigger_func(old, new, ignores):
 def _unimport_module(old, ignores):
     """Remove traces of a module"""
     __internal_swaprefs_ignore__ = "unimport_module"
-    oldItems = vars(old).items()
-    ignores += (id(oldItems),)    
+    oldValues = vars(old).values()
+    ignores += (id(oldValues),)    
 
     # Get filename used by python code
     filename = old.__file__
@@ -375,7 +388,7 @@ def _unimport_module(old, ignores):
     if fileext in (".pyo", ".pyc", ".pyw"):
         filename = filename[:-1]
 
-    for name, value in oldItems:
+    for value in oldValues:
         try: objfile = inspect.getsourcefile(value)
         except TypeError: objfile = ""
         
